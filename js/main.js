6a9f4acddb326a285a158e22022a26ecb3ce46c8
Vitória Morais <vitoriamoraisfaria@gmail.com>
2026-05-27 15:38:33 -0300

    Parte de adicionar valores finalizada, pelo o que parece.

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let numeros = []
+function adicionar(){
+    let txtnum = document.getElementById('txtnum')
+    let num = Number(txtnum.value)
+    let adicionados = document.getElementById('selnum')
+
+
+    if(num < 1 || num > 100){
+        window.alert(`Número ${num} é inválido!`)
+        return
+    }
+    if(!numeros.includes(num)){
+        numeros.push(num)
+        let item = document.createElement('option')
+        item.text = `Valor ${num} adicionado.`
+        adicionados.appendChild(item)
+    } else{
+        window.alert(`Número ${num} já foi adicionado na lista.`)
+        return
+    }
+    
+}
+
+
+function resultado(){
+
+}
